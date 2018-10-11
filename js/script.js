b6afc75dc9b9acae11e7d2d4a36619753ded56a2
Svein Are Danielsen <sveindani@gmail.com>
2018-10-11 21:54:05 +0200

    added background-field, and some small edits on the HTML

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 (function ($) {
 
-    let $result = $('#result');
+    /**
+     * Output field
+     *
+     * @type {*}
+     */
+    const $OUTPUT = $('#result');
 
+    /**
+     *  Generates Kirki code for the config
+     */
     function generate_config() {
         let id = $('#config_id').val();
         let capability = $('#config_capability').val();
@@ -12,9 +20,12 @@
         str += `\t'option_type'\t=> '${option_type}',\n`;
         str += ') );';
 
-        $result.text(str);
+        $OUTPUT.text(str);
     }
 
+    /**
+     *  Generates Kirki code for a panel
+     */
     function generate_panel() {
         let textdomain = $('#textdomain').val();
         let id = $('#panel_id').val();
@@ -28,9 +39,12 @@
         str += `\t'priority'\t=> ${priority},\n`;
         str += `) );`;
 
-        $result.text(str);
+        $OUTPUT.text(str);
     }
 
+    /**
+     *  Generates Kirki code for a section
+     */
     function generate_section() {
         let textdomain = $('#textdomain').val();
         let id = $('#section_id').val();
@@ -42,64 +56,185 @@
         let str = `Kirki::add_section( '${id}', array(\n`;
         str += `\t'title'\t\t=> esc_attr__( '${title}', '${textdomain}' ),\n`;
         str += `\t'description'\t=> esc_attr__( '${description}', '${textdomain}' ),\n`;
-        if ( panel.length > 0 ) {
+
+        if (panel.length > 0) {
             str += `\t'panel'\t\t=> '${panel}',\n`;
         }
+
         str += `\t'priority'\t=> ${priority},\n`;
         str += `) );`;
 
-        $result.text(str);
+        $OUTPUT.text(str);
     }
 
+    /**
+     * Get the field type selected
+     * @returns {*}
+     */
     function get_field_type() {
         return $('#field_type').val();
     }
 
+    /**
+     * Print the correct field type based on which field type is selected
+     */
     function show_field_type() {
         let type = get_field_type();
 
-        switch ( type ) {
+        switch (type) {
+            case 'background':
+                print_field_background();
+                break;
             case 'text':
-                show_field_type_text();
+                print_field_text();
                 return;
         }
     }
 
-    function show_field_type_text() {
-        $container = $('#field_container');
-
-        print_input( $container, 'Settings', 'field_settings', 'text' );
-        print_input( $container, 'Label', 'field_label', 'text' );
-        print_input( $container, 'Description', 'field_description', 'text' );
-        print_input( $container, 'Section', 'field_section', 'text' );
-        print_input( $container, 'Default', 'field_default', 'text' );
-        print_input( $container, 'Priority', 'field_priority', 'number' );
+    /**
+     * Print the default fields for every field
+     *
+     * @param {string} selector
+     */
+    function print_field_defaults(selector) {
+        print_input(selector, 'Settings', 'field_settings', 'text');
+        print_input(selector, 'Label', 'field_label', 'text');
+        print_input(selector, 'Description', 'field_description', 'text', 'Will not show in the output if empty.');
+        print_input(selector, 'Section', 'field_section', 'text');
+        print_input(selector, 'Priority', 'field_priority', 'number');
     }
 
-    function print_input( selector, label, id, inputType, description = '' ) {
+    /**
+     * Prints the fields for the text field
+     */
+    function print_field_text() {
+        let $container = $('#field_container');
+        $container.text('');
+
+        print_field_defaults($container);
+        print_input($container, 'Default', 'field_default', 'text');
+    }
+
+    /**
+     * Prints the fields for the background field
+     */
+    function print_field_background() {
+        let $container = $('#field_container');
+        $container.text('');
+
+        print_field_defaults($container);
+
+        print_div($container, 'col-12 mt-3', '<h3>Default:</h3>');
+
+        print_input($container, 'Background Color', 'field_background_color', 'color');
+        print_input($container, 'Background Image', 'field_background_image', 'text');
+        print_input($container, 'Background Repeat', 'field_background_repeat', 'select', '', [
+            'repeat',
+            'repeat-x',
+            'repeat-y',
+            'no-repeat',
+            'initial',
+            'inherit'
+        ]);
+        print_input($container, 'Background Position', 'field_background_position', 'select', '', [
+            'left top',
+            'left center',
+            'left bottom',
+            'right top',
+            'right center',
+            'right bottom',
+            'center top',
+            'center center',
+            'center bottom'
+        ]);
+        print_input($container, 'Background Size', 'field_background_size', 'select', '', [
+            'auto',
+            'cover',
+            'contain',
+            'initial',
+            'inherit'
+        ]);
+        print_input($container, 'Background Attachment', 'field_background_attachment', 'select', '', [
+            'scroll',
+            'fixed',
+            'local',
+            'initial',
+            'inherit'
+        ]);
+
+    }
+
+    /**
+     * Print an input field
+     *
+     * @param {string} selector
+     * @param {string} label
+     * @param {string} id
+     * @param {string} inputType
+     * @param {string} description
+     * @param {array} options
+     */
+    function print_input(selector, label, id, inputType, description = '', options = []) {
         let str = '';
         str += '<div class="col-3">';
         str += '<div class="form-group">';
-        str += '<label for="'+id+'"><strong>'+label+'</strong></label>';
-        str += '<input type="'+inputType+'" class="form-control" id="'+id+'" placeholder="'+label+'">';
-        if ( description !== '' ) {
-            str += '<small class="text-muted">'+description+'</small>';
+        str += '<label for="' + id + '"><strong>' + label + '</strong></label>';
+
+        // Options array is empty
+        if (options.length === 0) {
+            str += '<input type="' + inputType + '" class="form-control" id="' + id + '" placeholder="' + label + '">';
+        } else {
+            str += '<select name="' + id + '" id="' + id + '" class="form-control">';
+            let first = true;
+            // Loop through options
+            options.forEach(e => {
+                str += '<option value="' + e.toLowerCase() + '"  '+((first)?'selected':'')+'>' + e + '</option>';
+                first = false;
+            });
+            str += '</select>';
         }
+
+        // If there is a description
+        if (description !== '') {
+            str += '<small class="text-muted">' + description + '</small>';
+        }
+
         str += '</div><!-- /.form-group -->';
         str += '</div><!-- /.col-3 -->';
 
         selector.append(str);
     }
 
+    /**
+     * Prints a <div> with class and content
+     *
+     * @param {string} selector
+     * @param {string} _class
+     * @param {string} content
+     */
+    function print_div(selector, _class, content = '') {
+        selector.append('<div class="' + _class + '">' + content + '</div>');
+    }
+
+    /**
+     * Generates the correct field based on which field is selected
+     */
     function generate_field() {
         let type = get_field_type();
 
-        switch ( type ) {
+        switch (type) {
+            case 'background':
+                generate_field_background();
+                return;
             case 'text':
                 generate_field_text();
+                return;
         }
     }
 
+    /**
+     * Generates all the fields for a text-field
+     */
     function generate_field_text() {
         let textdomain = $('#textdomain').val();
         let settings = $('#field_settings').val();
@@ -113,21 +248,75 @@
         str += `\t'type'\t\t=> 'text',\n`;
         str += `\t'settings'\t=> '${settings}',\n`;
         str += `\t'label'\t\t=> esc_attr__( '${label}', '${textdomain}' ),\n`;
-        if ( description.length > 0 ) {
+
+        // If there is a description
+        if (description.length > 0) {
             str += `\t'description'\t=> '${description}',\n`;
         }
+
         str += `\t'section'\t=> '${section}',\n`;
         str += `\t'default'\t=> '${_default}',\n`;
         str += `\t'priority'\t=> ${prior},\n`;
         str += `) );`;
 
-        $result.text(str);
+        $OUTPUT.text(str);
     }
 
+    /**
+     * Generates all the fields for a background-field
+     */
+    function generate_field_background() {
+        let textdomain = $('#textdomain').val();
+        let settings = $('#field_settings').val();
+        let label = $('#field_label').val();
+        let description = $('#field_description').val();
+        let section = $('#field_section').val();
+        let prior = $('#field_priority').val();
+
+        let bg = {
+            color: $('#field_background_color').val(),
+            image: $('#field_background_image').val(),
+            repeat: $('#field_background_repeat').val(),
+            position: $('#field_background_position').val(),
+            size: $('#field_background_size').val(),
+            attachment: $('#field_background_attachment').val()
+        };
+
+
+        let str = `Kirki::add_field( '${textdomain}', array(\n`;
+        str += `\t'type'\t\t=> 'background',\n`;
+        str += `\t'settings'\t=> '${settings}',\n`;
+        str += `\t'label'\t\t=> esc_attr__( '${label}', '${textdomain}' ),\n`;
+
+        if (description.length > 0) {
+            str += `\t'description'\t=> esc_attr__( '${description}', '${textdomain}' ),\n`;
+        }
+
+        str += `\t'section'\t=> '${section}',\n`;
+        str += `\t'priority'\t=> ${prior},\n`;
+        str += `\t'default'\t=> array(\n`;
+        str += `\t\t'background-color'\t=> '${bg.color}'\n`;
+        str += `\t\t'background-image'\t=> '${bg.image}'\n`;
+        str += `\t\t'background-repeat'\t=> '${bg.repeat}'\n`;
+        str += `\t\t'background-position'\t=> '${bg.position}'\n`;
+        str += `\t\t'background-size'\t=> '${bg.size}'\n`;
+        str += `\t\t'background-attachment'\t=> '${bg.attachment}'\n`;
+        str += `\t),`;
+        str += `) );`;
+
+        $OUTPUT.text(str);
+    }
+
+    /**
+     * Event listeners
+     */
     $('#generate_config').on('click', generate_config);
     $('#generate_panel').on('click', generate_panel);
     $('#generate_section').on('click', generate_section);
     $('#field_type').on('change', show_field_type);
     $('#generate_field').on('click', generate_field);
+
+    // Show the base selected field on the field-tab
+    show_field_type();
 
 })(jQuery);
